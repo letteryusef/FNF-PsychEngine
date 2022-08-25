abf49837c1381007c72641fd63a85f1c16054593
Yusef Lee <yusefleeb@gmail.com>
2022-08-25 18:00:05 -0300

    animated icon??

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -5,14 +5,21 @@ import flixel.FlxSprite;
 import flixel.graphics.FlxGraphic;
 import openfl.utils.Assets as OpenFlAssets;
 
+#if sys
+import sys.io.File;
+import sys.FileSystem;
+#end
+
 using StringTools;
 
 class HealthIcon extends FlxSprite
 {
 	public var sprTracker:FlxSprite;
+	private var isAnimated:Bool = false;
 	private var isOldIcon:Bool = false;
 	private var isPlayer:Bool = false;
 	private var char:String = '';
+	private var xmlContent:String;
 	private var leWidth:Float;
 
 	public function new(char:String = 'bf', isPlayer:Bool = false)
@@ -46,26 +53,44 @@ class HealthIcon extends FlxSprite
 		 	if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
 		
-			var iconGraphic:FlxGraphic = Paths.image(name);
-
-			switch (iconGraphic.width)
-		    {
-				case 450: leWidth = 3;
-				case 300: leWidth = 2;
-				case 150: leWidth = 1;
-				default: leWidth = 2;
-			}
-
-			loadGraphic(iconGraphic); //Load stupidly first for getting the file size
-			if (leWidth != 1)
+			#if sys
+			if (FileSystem.exists('images/' + name + '.xml'))
 			{
-				loadGraphic(iconGraphic, true, Math.floor(width / leWidth), Math.floor(height)); //Then load it fr
-			    iconOffsets[0] = (width - 150) / leWidth;
-			    iconOffsets[1] = (width - 150) / leWidth;
+				isAnimated = true;
+				xmlContent = File.getContent('images/' + name + '.xml');
 			}
-			animation.add('idle', [0], 0, false, isPlayer);
-			animation.add('losing', leWidth >= 2 ? [1] : [0], 0, false, isPlayer);
-			animation.add('winning', leWidth == 3 ? [2] : [0], 0, false, isPlayer);
+			#end
+
+			if (isAnimated){
+				var leAnimation:Dynamic = Paths.image(name);
+				loadGraphic(leAnimation);
+
+				animation.addByPrefix('idle', char + 'IDLE', 24, true, isPlayer);
+				animation.addByPrefix('losing', xmlContent.contains(char + 'LOSING') ? char + 'LOSING' : char + 'IDLE', 24, true, isPlayer);
+				animation.addByPrefix('winning', xmlContent.contains(char + 'WINNING') ? char + 'WINNING' : char + 'IDLE', 24, true, isPlayer);
+
+			} else {
+				var iconGraphic:FlxGraphic = Paths.image(name);
+
+				switch (iconGraphic.width)
+		   	    {
+					case 450: leWidth = 3;
+					case 300: leWidth = 2;
+					case 150: leWidth = 1;
+					default: leWidth = 2;
+				}
+
+				loadGraphic(iconGraphic); //Load stupidly first for getting the file size
+				if (leWidth != 1)
+				{
+					loadGraphic(iconGraphic, true, Math.floor(width / leWidth), Math.floor(height)); //Then load it fr
+			    	iconOffsets[0] = (width - 150) / leWidth;
+			    	iconOffsets[1] = (width - 150) / leWidth;
+				}
+				animation.add('idle', [0], 0, false, isPlayer);
+				animation.add('losing', leWidth >= 2 ? [1] : [0], 0, false, isPlayer);
+				animation.add('winning', leWidth == 3 ? [2] : [0], 0, false, isPlayer);
+			}
 			updateHitbox();
 			animation.play('idle');
 			this.char = char;
