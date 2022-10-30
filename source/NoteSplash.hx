0c9bd860c36d691788e29ecfa54972d9db95c358
Yusef Lee <yusefleeb@gmail.com>
2022-10-30 19:50:05 -0300

    pixel notesplashes

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -27,7 +27,11 @@ class NoteSplash extends FlxSprite
 	}
 
 	public function setupNoteSplash(x:Float, y:Float, note:Int = 0, texture:String = null, hueColor:Float = 0, satColor:Float = 0, brtColor:Float = 0) {
-		setPosition(x - Note.swagWidth * 0.95 * PlayState.instance.leSplashOffsetX, y - Note.swagWidth * PlayState.instance.leSplashOffsetY);
+		if(PlayState.isPixelStage) {
+			setPosition(x + 30, (y + Note.swagWidth) / 2);
+		} else {
+			setPosition(x - Note.swagWidth * 0.95 * PlayState.instance.leSplashOffsetX, y - Note.swagWidth * PlayState.instance.leSplashOffsetY);
+		}
 		alpha = PlayState.instance.splashAlpha;
 
 		if(texture == null) {
@@ -54,20 +58,32 @@ class NoteSplash extends FlxSprite
 	}
 
 	function loadAnims(skin:String) {
-		frames = Paths.getSparrowAtlas(skin);
-		if (PlayState.instance.splashRandom)
-		{
-			for (i in 1...3) {
-				animation.addByPrefix("note1-" + i, PlayState.instance.noteSplashName + " blue " + i, 24, false);
-				animation.addByPrefix("note2-" + i, PlayState.instance.noteSplashName + " green " + i, 24, false);
-				animation.addByPrefix("note0-" + i, PlayState.instance.noteSplashName + " purple " + i, 24, false);
-				animation.addByPrefix("note3-" + i, PlayState.instance.noteSplashName + " red " + i, 24, false);
-			}
+		if(PlayState.isPixelStage) {
+			loadGraphic(Paths.image('pixelUI/' + skin));
+			width = width / 8;
+			height = height / 4;
+			loadGraphic(Paths.image('pixelUI/' + skin), true, Math.floor(width), Math.floor(height));
+
+			antialiasing = false;
+			setGraphicSize(Std.int(width * PlayState.daPixelZoom));
+
+			animation.add("note0-1", [0, 1, 2, 3], 12, false);
+			animation.add("note0-2", [4, 5, 6, 7], 12, false);
+			animation.add("note1-1", [8, 9, 10, 11], 12, false);
+			animation.add("note1-2", [12, 13, 14, 15], 12, false);
+			animation.add("note2-1", [16, 17, 18, 19], 12, false);
+			animation.add("note2-2", [20, 21, 22, 23], 12, false);
+			animation.add("note3-1", [24, 25, 26, 27], 12, false);
+			animation.add("note3-2", [28, 29, 30, 31], 12, false);
 		} else {
-			animation.addByPrefix("note1", PlayState.instance.noteSplashName + " blue", 24, false);
-			animation.addByPrefix("note2", PlayState.instance.noteSplashName + " green", 24, false);
-			animation.addByPrefix("note0", PlayState.instance.noteSplashName + " purple", 24, false);
-			animation.addByPrefix("note3", PlayState.instance.noteSplashName + " red" , 24, false);
+			frames = Paths.getSparrowAtlas(skin);
+			scale.set(1, 1);
+			for (i in 1...3) {
+				animation.addByPrefix("note0-" + i, "note splash purple " + i, 24, false);
+				animation.addByPrefix("note1-" + i, "note splash blue " + i, 24, false);
+				animation.addByPrefix("note2-" + i, "note splash green " + i, 24, false);
+				animation.addByPrefix("note3-" + i, "note splash red " + i, 24, false);
+			}
 		}
 	}
 
